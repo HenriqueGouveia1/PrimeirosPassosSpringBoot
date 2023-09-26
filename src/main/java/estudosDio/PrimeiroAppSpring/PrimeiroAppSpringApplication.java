d4d081f67808e8e612b69c3f121a4d49e7273b36
henriquegouveia1 <henriquegs002@gmail.com>
2023-09-26 10:02:42 -0300

    Iniciando repositorio com uma controller simples

---
+++ src/main/java/estudosDio/PrimeiroAppSpring/PrimeiroAppSpringApplication.java
@@ -0,0 +1,14 @@
+package estudosDio.PrimeiroAppSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroAppSpringApplication.class, args);
+	}
+
+}
+
